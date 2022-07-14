4dd647c27e80405c71c96a4e9b3949ceeddfa5df
Evgeniya <evgeniya-e92@mail.ru>
2022-07-14 16:04:57 +0300

    Bonus commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 14200; //цена билета
+        int y = 20; //количество рублей за одну милю
+
+        System.out.println(x / y);
+         int q = 710; // количество полученных миль
+
+    }
+}
